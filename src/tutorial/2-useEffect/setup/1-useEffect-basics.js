77632e64f4b4ad021b7d191b55d068594a9284d3
Isaac3N <theisaac3n@gmail.com>
2021-03-12 12:25:47 +0100

    github users

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,24 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0)
+  useEffect(()=>{
+    console.log('this is a use effect')
+    if (value > 0)
+    {document.title = (`This is the ${value} message`)}
+  }, [value])
+  useEffect(()=> {
+    console.log('hello world')
+  }, [])
+  console.log('rendered component')
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={()=> {
+        setValue(value+1)
+      }}>Click me</button>
+    </>
+    );
 };
 
 export default UseEffectBasics;
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,25 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth)
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+  useEffect(()=> {
+    window.addEventListener('resize', checkSize)
+    return()=> {
+      console.log('cleanup')
+      window.removeEventListener('resize', checkSize)
+    }
+  })
+  // a cleanup function is one where you have to remove the event listener that was added so as to
+  // messy code when the useEffect is being rendered 
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{size}px</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
