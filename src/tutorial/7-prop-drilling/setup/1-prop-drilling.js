7c95c70ade1ebcc2723715f0bf2f6a4e84baac74
Isaac3N <theisaac3n@gmail.com>
2021-03-14 22:58:06 +0100

    context api

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -6,27 +6,35 @@ import {data} from '../../../data'
 
 const PropDrilling = () => {
   const[people, setPeople] = useState(data)
+  const removePerson = (id) => {
+    setPeople((people)=>{
+      return people.filter((person) => person.id !== id)
+    })
+  }
   return (
     <section>
       <h3>prop drilling</h3>
-      <List people={people}/>
+      <List people={people} removePerson={removePerson}/>
     </section>
   );
 };
-const List = ({people}) => {
+const List = (props) => {
+  const {people, removePerson} = props
   return (
     <>
-      {people.map((person)=> {
-        return <SinglePerson key={person.id }/>
+      {people.map((i)=> {
+        return <SinglePerson key={i.id} {...i} removePerson={removePerson}/>
       })}
     </>
   )
 }
 
-const SinglePerson = ({id,name}) => {
+const SinglePerson = (props) => {
+  const {id,name, removePerson} = props 
   return (
     <div className="item">
       <h4>single item</h4>
+      <button onClick={() => removePerson(id)}>remove</button>
     </div>
   )
 }
