15d0bbd732a4b72725211313ca47e9af13bfc0c1
Isaac3N <theisaac3n@gmail.com>
2021-03-14 19:21:20 +0100

    short circuit evaluation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const [text, setText] = useState('Isaac')
+  const[isError, setIsError] = useState(false)
   const firstValue = text || 'hello world';
   const secondValue = text && 'hello world';
 
@@ -12,6 +13,14 @@ const ShortCircuit = () => {
       {/*<h1>{firstValue}</h1>
   <h1>value: {secondValue}</h1>*/}
       {text && <h1>Hello world</h1>}
+      <button className='btn' onClick={()=> setIsError(!isError)}>toggle error</button>
+      {isError && <h1>Error...</h1>}
+      {isError ? <p> There is an error </p>: 
+      <div>
+        <h2>There is no error</h2> 
+      </div>
+      
+      }
     </>
   )
 };
